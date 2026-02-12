7f5638f612c166c652ed36f5d93b338985620077
mohamedelking1 <mohamed0113326657@gmail.com>
2026-02-12 11:33:22 +0200

    Bind server to all network interfaces

--- server-public.js
+++ server-public.js
@@ -11,6 +11,6 @@ app.get("/", (req, res) => {
   res.send("AI Creator Server Running 🚀");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port:", PORT);
 });
